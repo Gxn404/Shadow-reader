c1df1292fb2f624d099b49f0b059531345682f0c
GAGAN SHETTY <144126593+ShadowmaxCoder@users.noreply.github.com>
2024-11-23 20:05:49 +0530

    Update search.js

--- api/manga/search.js
+++ api/manga/search.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import axios from "axios";
 import NodeCache from "node-cache";
 import cors from "cors";
@@ -41,47 +41,4 @@ const handler = async (req, res) => {
 };
 
 export default handler;
-=======
-import axios from "axios";
-import NodeCache from "node-cache";
-import cors from "cors";
 
-const cache = new NodeCache({ stdTTL: process.env.CACHE_TTL || 600, checkperiod: 300 });
-const MANGADX_API_URL = process.env.MANGADX_API_URL || "https://api.mangadex.org";
-
-const handler = async (req, res) => {
-  cors()(req, res, async () => {
-    const query = req.query.query || "";
-
-    if (!query) {
-      return res.status(400).json({ error: "Query parameter is required" });
-    }
-
-    const cacheKey = `search-${query}`;
-    const cachedData = cache.get(cacheKey);
-
-    if (cachedData) {
-      return res.json(cachedData);
-    }
-
-    try {
-      const response = await axios.get(`${MANGADX_API_URL}/manga`, { params: { title: query } });
-
-      const mangaList = response.data.data.map((manga) => ({
-        id: manga.id,
-        title: manga.attributes.title,
-        description: manga.attributes.description,
-        status: manga.attributes.status,
-        year: manga.attributes.year,
-      }));
-
-      cache.set(cacheKey, mangaList);
-      return res.json(mangaList);
-    } catch (error) {
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  });
-};
-
-export default handler;
->>>>>>> 1f0d6e76c56170febf855ad016dcaeb4bea37b11
